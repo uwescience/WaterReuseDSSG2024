ebdbd43afb7e5594af8841d510fb3709f7833352
Nora Povejsil <npovejsil@berkeley.edu>
2024-07-12 16:26:28 -0700

    this one is the one with the updates with crosswalk_raster

--- scripts/crosswalk/general-crosswalk.R
+++ scripts/crosswalk/general-crosswalk.R
@@ -8,18 +8,37 @@ source("./map_utils/crosswalks.R")
 
 # source()
 
-crosswalk <- function(data = NULL, 
+crosswalk <- function(data, 
+                      data2 = NULL,
                       shapefile = NULL, 
+                      raster = NULL,
                       source_scale = NULL, 
-                      key = NULL, 
                       target_scale = NULL,
-                      typeof) {
+                      key = NULL, 
+                      data_type = NULL, 
+                      method = "area", 
+                      output_type = NULL
+                      ) {
   # Args: 
+    # data: can be labeled data with ID location column(s), point data (contains lat/lon columns), a shapefile, or a raster
+    # data_type: type of "data." Pick one of these options: "points", "shapes", "raster".
+    # data2: a non-shapefile and non-raster dataset. Must contain the same key as data if key argument is not NULL. 
+    # shapefile: a shapefile containing target geometries, to be combined with data 
+    # raster: a raster to be combined with data 
+    # source_scale: the location column name(s) of the orginal data. List type..? 
+    # target_scale: the location column name(s) of the data to be crosswalked. List type..? 
+    # key: a numeric key identifier for crosswalking to a new census data scale or between datasets. 
+    # method: pick between "area" or "population" to preserve when doing aggregations on datasets. Default area prioritization. 
+    # output_type: relevant if raster is not NULL. Options are "raster" or "shapefile" when doing raster/shapefile crosswalks. 
     # source_scale: if data contains points given by latitude and longitude, source scale must contain those column names c("lat", "lon")
+  # Output: 
+    # A dataframe on the target geographic scale. At this time, the user may need to perform weighted aggregations for moving up in scale and apply weights to splits. 
   
-  if (is.null(shapefile) && !is.null(key) && typeof == "ID") {
+  if (is.null(shapefile) && !is.null(key)) {
     
-    crosswalk_data(data = data, 
+    #check if Jihyeon agrees about need for data2 argument
+    crosswalk_data(data = data,
+                   data2 = data2,
                    source_scale = source_scale,
                    key = key, 
                    target_scale = target_scale)
@@ -31,6 +50,15 @@ crosswalk <- function(data = NULL,
                    data = data, 
                    location_columns = source_scale,
                    type = typeof)
+  }
+  
+  else if (!is.null(raster)) {
+    
+    crosswalk_raster(data1 = raster, 
+                     data2 = data, 
+                     method = method, 
+                     output_type = output_type, 
+                     points = data_type)
   }
   
   else {
