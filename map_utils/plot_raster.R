e07e487f1f412537c76a493100c4d5f3b0185fd2
Mbye Sallah <mbyesallah17@gmail.com>
2024-07-15 16:15:18 -0700

    Remove grids on plots

--- map_utils/plot_raster.R
+++ map_utils/plot_raster.R
@@ -1,7 +1,6 @@
 # This function plot raster datasets
 
-plot_raster <- function(data, layer, 
-                            shapefile, cellsize, map_title) {
+plot_raster <- function(data, layer, map_title) {
   if (!inherits(data, "SpatRaster")) {
     stop("Input data is not a SpatRaster object")
   }
@@ -23,29 +22,10 @@ plot_raster <- function(data, layer,
   raster_df <- as.data.frame(raster_layer, xy = TRUE)
   colnames(raster_df)[3] <- "value"
   
-  # # Convert polygon to a data frame for ggplot
-  # grid_df <- as.data.frame(grid_data)
- 
-  # Create a grid over the bounding box of your shapefile data
-  grid <- if (is.null(cellsize)) {
-    st_make_grid(shapefile)
-  } else {
-    st_make_grid(shapefile, cellsize = cellsize)
-  }
-  
-  # Convert the grid to an sf object
-  grid_sf <- st_sf(geometry = grid)
-  
-  
-  # Check if CRS match, if not transform
-  if (st_crs(data) != st_crs(grid_sf)){
-    grid_sf <- st_transform(grid_sf, st_crs(data))
-    }
   
   # Plot using ggplot2
   raster_plot <- ggplot() +
     geom_raster(data = raster_df, aes(x = x, y = y, fill = value)) +
-    geom_sf(data = grid_sf, fill = NA, color = "lightblue") +
     scale_fill_viridis_c() +
     labs(title = map_title, x = "Longitude", 
          y = "Latitude", fill = "Value") +
