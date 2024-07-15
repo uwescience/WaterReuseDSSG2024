3c4f03daa8b640e963fbfcee40d0245e5ff4231b
Mbye Sallah <mbyesallah17@gmail.com>
2024-07-15 14:05:52 -0700

    Included raster plots and called ggsave only once

--- map_utils/mapper.R
+++ map_utils/mapper.R
@@ -7,7 +7,6 @@ mapper <- function(data = NULL,
                    variable_name = NULL,
                    data_key = NULL, 
                    shape_key = NULL, 
-                   plot_all = FALSE, 
                    plot = NULL, 
                    map_path, 
                    map_title = NULL,
@@ -19,19 +18,21 @@ mapper <- function(data = NULL,
                    map_font = "Arial",
                    base_unit = NULL,
                    name = NULL,
-                   bbox = NULL){
+                   bbox = NULL,
+                   layer = NULL,
+                   cellsize = NULL){
   # data: The dataset that contains the column of interest
   # shapefile: A shapefile with FIPS
   # location: a vector of column names of the coordinates
   # variable_name: A column name of the variable of interest.
   # data_key: A key in the data table that can be used to merge with the shapefiles e.g. FIPS
   # shape_key: A key in the shapefile use to my with data e.g. GEOID
-  # plot_all: if TRUE, it maps both the choropleth and points if all the required variables are available
-  # plot: Takes either "choropleth" or "points" to specify either points or choropleth
+  # plot: Takes either "choropleth", "points", or  "raster" to specify either points or choropleth
   # map_path: Specify a folder path where you will save the maps
   # map_title: A string of the title of the map.
   # base_unit: a string with the base unit to be drawn on the usmap e.g 'counties'
   # bbox: a vector of bounding box coordinates (xmin, ymin, xmax, ymax) to crop the shapefile
+  # layer: The layer to be plotted in multilayer rasters
   
   # Get the required packages and functions
   require(tidyverse)
@@ -39,6 +40,7 @@ mapper <- function(data = NULL,
   source("map_utils/map_utils.R")
   source("map_utils/get_shapes.R")
   source("map_utils/map_points.R")
+  source("map_utils/plot_raster.R")
   
   
   # crop the shapefile if the bbox is not NULL
@@ -52,53 +54,30 @@ mapper <- function(data = NULL,
     # Get the intersection of the shapefile and bbox
     shapefile = st_intersection(shapefile, bbox_sf)
   }
-  
-  if (plot_all == TRUE){
-    if (is.null(location_columns)){
-      choro_map = map_choropleth(data, shapefile, data_key, 
-                                 shape_key, variable = variable_name,
-                                 map_title, map_caption="Caption",
-                                 map_percentile, low_color, 
-                                 high_color, na_color, map_font)
-      ggsave(filename = paste0(variable_name, ".png"), plot = choro_map,
-             path = map_path, device = "png")
-      print("The coordinate are needed to plot the points")
-    } else {
-      choro_map = map_choropleth(data, shapefile, data_key, 
-                                 shape_key, variable = variable_name,
-                                 map_title, map_caption="Caption",
-                                 map_percentile, low_color, 
-                                 high_color, na_color, map_font)
-      point_map = map_points(shapefile, data, location_columns, 
-                             variable_name, base_unit, map_title, name, bbox)
-      ggsave(filename = paste0(variable_name, ".png"), 
-             plot = choro_map, path = map_path, device = "png")
-      ggsave(filename = paste0(variable_name, ".png"), 
-             plot = point_map, path = map_path, device = "png")
-      
-    }
-    
-  } else if (tolower(plot) == "choropleth"){
-    choro_map = map_choropleth(data, shapefile, data_key, 
-                               shape_key, variable = variable_name,
-                               map_title, map_caption="Caption",
-                               map_percentile, low_color, 
-                               high_color, na_color, map_font)
-    ggsave(filename = paste0(variable_name, ".png"),
-           plot = choro_map, path = map_path, device = "png")
-    
-  } else if (tolower(plot) == "points"){
-    point_map = map_points(shapefile, data, 
-                           location_columns, variable_name,
-                           base_unit, map_title, name, bbox)
-    
-    ggsave(filename = paste0(variable_name, ".png"), 
-           plot = point_map, path = map_path, device = "png")    
-    
+
+    # plot choropleth map
+  if (tolower(plot) == "choropleth") {
+    plot_to_save <- map_choropleth(data, shapefile, data_key, 
+                                   shape_key, variable = variable_name,
+                                   map_title, map_caption,
+                                   map_percentile, low_color, 
+                                   high_color, na_color, map_font)
+  } else if (tolower(plot) == "points") {
+    plot_to_save <- map_points(shapefile, data, 
+                               location_columns, variable_name,
+                               base_unit, map_title, name, bbox)
+  } else if (tolower(plot) == "raster") {
+    plot_to_save <- plot_raster(data, layer, shapefile, cellsize)
   } else {
     print("The map you specified cannot be plotted at the moment.")
+    return(NULL) # Exit function early if the plot type is not supported
   }
   
+  ggsave(filename = ifelse(is.null(variable_name), paste0(plot, ".png"), paste0(variable_name, ".png")),
+         plot = plot_to_save, path = map_path, device = "png")
+  
+  # Return the ggplot object
+  return(plot_to_save)
+  
 }
-
 
