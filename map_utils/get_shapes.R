3592ec04a2b63e5f85158a14bcf0528b5cbe8d86
Mbye Sallah <mbyesallah17@gmail.com>
2024-07-16 16:07:15 -0700

    Wrote a documentation for the shapefiles

--- map_utils/get_shapes.R
+++ map_utils/get_shapes.R
@@ -1,11 +1,23 @@
 # I am writing a function that assigns locations to a shape base on the given longitudes and latitudes
 
-get_shapes <- function(dataset, longitudes, latitudes, census_year = "2023"){
+#' Assign shapefiles using dat
+#'
+#' @param dataset The dataset that contains the column of interest
+#' @param longitudes A string of the longitude column name
+#' @param latitudes A string of the longitude column name
+#' @param census_year An integer of the census year of the shapefile generated using tigris packahge
+#' @param filter_by A vector of boundary determining the bounding box 
+#' @return A dataframe of shapefiles including the FIPS code
+#' @export
+#'
+#' @examples
+get_shapes <- function(dataset, longitudes, latitudes, census_year = "2023", 
+                       filter_by = NULL){
   # Load the libraries
   require(tigris)
   require(sf)
   # get a county shapefile using tigris
-  us_counties = counties(year = census_year, filter_by = c(-124.7844079, -66.9513812, 24.7433195, 49.3457868))
+  us_counties = counties(year = census_year, filter_by = filter_by)
 
   # Convert the dataframe to a spatial object and use the us_counties' crs
   points <- st_as_sf(dataset, coords = c(longitudes, latitudes), crs = st_crs(us_counties))
