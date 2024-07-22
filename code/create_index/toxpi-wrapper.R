03587f66773629985955b4c3b7c79b04b838fac2
Jihyeon Bae <jihyeonb@uw.edu>
2024-07-22 14:17:26 -0700

    docstringed create_index and check_id functions in toxpi_wrapper.R

--- code/create_index/toxpi-wrapper.R
+++ code/create_index/toxpi-wrapper.R
@@ -1,19 +1,15 @@
+
 check_id <- function(data, id_var) {
-  
-  #' Checks if the ID column is valid
-  #' 
+  #' Checks if the ID is valid
+  #' @description 
   #' Checks that the user-provided ID column is in the dataframe passed to create_index()
   #' 
-  #' Inputs:
-  #' 
-  #'  - data (dataframe) - dataframe the user wants to create a toxpiR index from
-  #'  - id_var (string) - name of the column in data that identifies the basic unit over which
-  #'    the index is contstructed; for example, in an index of drought risk over US counties
-  #'    the index might be 'county_id'
-  #' 
-  #' Output:
-  #'    (boolean) - is the id valid (is it in the dataframe)?
-    
+  #' @param data dataframe the user wants to create a toxpiR index from
+  #' @param id_var string. name of the column in data that identifies the basic unit over which the index is contstructed; 
+  #' for example, in an index of drought risk over US counties
+  #' the index might be 'county_id'
+  #' @returns output type boolean - is the id valid (is it in the dataframe)?
+
   if (sum(id_var %in% names(data)) == 0) {
     stop("The id passed to create_index() has to be one of the columns in the dataset")
   }
@@ -52,40 +48,39 @@ create_index <- function(df, id_col, weights, index_name = "index_value") {
     #' wrapper around toxpiR; takes a dataframe, weights, and a designated column specifying the id; 
     #' calculates a toxpiR index that is ready for mapping using the functions in map_utils
     #' 
-  #' Inputs:
-  #' - data (dataframe) - expects a dataframe containing a variable of
+  #' @param data (dataframe) - expects a dataframe containing a variable of
   #'   interest, indexed by a geographic region identifier like FIPS
   #'
-  #' - shape (dataframe) - a shapefile that specifies the
+  #' @param shape (dataframe) - a shapefile that specifies the
   #'   geographic regions in question; must contain a 'geometry' column
   #'
-  #' - data_key (string) - a string that contains the NAME of column in
+  #' @param data_key (string) - a string that contains the NAME of column in
   #'   data that should be used to merge it with the shapefile. This
   #'   should be something like "FIPS"
   #'
-  #' - shape_key (string) - a string that contains the NAME of column in
+  #' @param shape_key (string) - a string that contains the NAME of column in
   #'   the shapefile that should be used to merge it with the data. This
   #'   should be something like "FIPS"
   #'
-  #' - variable (string) - a string containing the NAME of the column in
+  #' @param variable (string) - a string containing the NAME of the column in
   #'   data that contains the variable that should be used to make the map
   #'
-  #' - map_title (string) - the map title
+  #' @param map_title (string) - the map title
   #'
-  #' - map_caption (string) - a string defining the caption that appears
+  #' @param map_caption (string) - a string defining the caption that appears
   #'   at the bottom right of the map figure
   #'
-  #' - map_percentile (boolean) - whether the map should display raw values
+  #' @param map_percentile (boolean) - whether the map should display raw values
   #'   of variable (=FALSE, default) or show which percentile of the
   #'   data distribution the geographic unit in question falls into (=TRUE)
   #'
-  #' - low_color, high_color, na_color (strings) - the colors designating
+  #' @param low_color, high_color, na_color (strings) - the colors designating
   #'   the lowest and highest values of variable, respectively, and the
   #'   color that should indicate missing data
   #'
-  #' - map_font (string) - font in the title, caption, legends
+  #' @param map_font (string) - font in the title, caption, legends
   #'
-  #' Outputs:
+  #' @Outputs:
   #' - a ggplot2 object mapping the shapes defined in the dataset and
   #'   color-coding them according to the value of variable; instead of
   #'   plotting the RAW value of variable, this function converts them
