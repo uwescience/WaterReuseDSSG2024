0367eeb0bc77e1a8050745f7155b9e3a4d74bc66
Jihyeon Bae <jihyeonb@uw.edu>
2024-07-23 16:57:05 -0700

    initial test run renegerating cvi. updating get_pca_weights.R function

--- code/create_index/weighted_average.R
+++ code/create_index/weighted_average.R
@@ -2,7 +2,7 @@ get_weighted_average <- function(df,
                                    weights,
                                    excluded_cols) {
   data_matrix <- df[ , !(names(df) %in% excluded_cols)]
-  
+  print(dim(data_matrix))
   M <- as.matrix(data_matrix)
   
   return(M %*% weights)
