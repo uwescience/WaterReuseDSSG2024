68522e76642f01c2d9c2463d0fd05bb323b30f98
Jihyeon Bae <jihyeonb@uw.edu>
2024-07-26 16:32:32 -0700

    first draft of apply_weight.R function

---
+++ code/crosswalk/apply_weight.R
@@ -0,0 +1,82 @@
+
+apply_weight <- function(data, source_scale = NULL, 
+                         weight_data = NULL, key = NULL, target_scale = NULL, 
+                         variable = NULL,
+                         method = NULL, weight_value = NULL) {
+  library(dplyr)
+  library(docstring)
+  #' apply_weight
+  #' @description This function will assign weights to the data frame. If weights are used to 
+  #' @param data dataframe that contains the target scale
+  #' @param weight_data dataframe that contains weights assigned to target scale
+  #' @param source_scale character vector. name of the source_scale in data
+  #' @param key character vector. name of the weight_data that matches with the source_scale in data
+  #' @param target_scale character vector. source scale to convert into. target_scale should be available in the weight_data.
+  #' @param weight_value character vector. name of the variable that contains weight values
+  #' @param method aggregation or apportioning method. "weighted_mean", "weighted_sum", "sum", and "mean"
+  #' @param variable variable of interest. can be a list of multiple variables
+  #' @examples apply_weight( data = cvi, 
+  #' weight_data = tr_zc_area, 
+  #' source_scale = "FIPS Code", 
+  #' key = "tract.census.geoid",
+  #' target_scale = "county.census.geoid",
+  #' weight_value = "afact",
+  #' method = "weighted_sum",
+  #' variable = c("CVI_overall", "CVI_base_all"))
+  #' 
+
+  
+  if (!is.character(data[[source_scale]])) {
+    data <- data %>%
+      mutate({{ source_scale }} := as.character({{ source_scale }}))
+  }
+  
+  joined_data <- data %>%
+    left_join(weight_data, by = setNames(key, source_scale))
+  print(names(joined_data))
+  
+  if (tolower(method) == "weighted_sum") {
+    processed_data <- joined_data %>%
+      group_by( {{ target_scale }} ) %>%
+      mutate(across(all_of(variable), 
+                    ~ sum(.x * 'weight_value', na.rm = TRUE),
+                    .names = "{.col}_weighted.sum")) 
+    
+  } else if (tolower(method) == "weighted_mean") {
+    processed_data <- joined_data %>%
+      group_by({{ target_scale }} ) %>%
+      mutate(across(all_of(variable), 
+                    ~ weighted.mean(.x, weight_value, na.rm = TRUE),
+                    .names = "{.col}_weighted.mean"))
+    
+  } else if (tolower(method) == "sum") {
+    processed_data <- joined_data %>%
+      group_by( {{ target_scale }} ) %>%
+      mutate(across({{ variable }},  
+                    ~ sum(., na.rm = TRUE),
+                    .names = "{.col}_sum"))
+    
+  } else if (tolower(method) == "mean") {
+    processed_data <- joined_data %>%
+    group_by( {{ target_scale }} ) %>%
+      mutate(across({{ variable }}, 
+                    ~ mean(., na.rm = TRUE),
+                    .names = "{.col}_mean"))
+    
+  } else {
+    stop("Invalid value for 'method'. Please specify either 'weighted_mean', 'weighted_sum', 'mean', or 'sum'.")
+  }
+  
+  return(processed_data)
+}
+docstring(apply_weight)
+
+apply_weight(data = cvi, 
+              weight_data = tr_ct_area, 
+              source_scale = "FIPS Code", 
+              key = "tract.census.geoid",
+              target_scale = "county.census.geoid",
+              weight_value = "afact",
+              method = "weighted_mean",
+              variable = c("CVI_overall", "CVI_base_all"))
+
---
+++ code/crosswalk/crosswalk_census.R
@@ -0,0 +1,148 @@
+source("code/crosswalk/apply_weight.R")
+
+# Read data files
+ct_tr_population <- read.csv("./code/crosswalk/weights_data/2020ct_tr.csv", header = TRUE)[-1,] 
+ct_tr_area <- read.csv("./code/crosswalk/weights_data/2020ct_tr_area.csv", header = TRUE)[-1,] 
+tr_ct_population <- read.csv("./data/2020tr_ct.csv", header = TRUE)[-1,]
+tr_ct_area <- read.csv("./data/2020tr_ct_area.csv", header = TRUE)[-1,]
+
+zc_tr_population <- read.csv("./code/crosswalk/weights_data/2020zc_tr.csv", header = TRUE)[-1,] 
+zc_tr_area <- read.csv("./code/crosswalk/weights_data/2020zc_tr_area.csv", header = TRUE)[-1,] 
+tr_zc_population <- read.csv("./code/crosswalk/weights_data/2020tr_zc_area.csv", header = TRUE)[-1,] 
+tr_zc_area <- read.csv("./code/crosswalk/weights_data/2020tr_zc_area.csv", header = TRUE)[-1,] 
+
+pm_tr_population <- read.csv("./code/crosswalk/weights_data/2020pm_tr.csv", header = TRUE)[-1,] 
+pm_tr_area <- read.csv("./code/crosswalk/weights_data/2020pm_tr_area.csv", header = TRUE)[-1,] 
+tr_pm_population <- read.csv("./code/crosswalk/weights_data/2020tr_pm.csv", header = TRUE)[-1,]
+tr_pm_area <- read.csv("./code/crosswalk/weights_data/2020tr_pm_area.csv", header = TRUE)[-1,]
+
+# Process data function
+process_data <- function(data) {
+  if ("tract" %in% names(data)) {
+    data$tract <- gsub("\\.", "", data$tract)
+  } 
+  if ("county" %in% names(data) && "tract" %in% names(data)) {
+    data <- data %>%
+      mutate(tract.census.geoid = paste0(county, tract))
+  } 
+  if ("county" %in% names(data) ) {
+    data <- data %>%
+      rename_with(~ifelse(. %in% "county", "county.census.geoid", .),
+                  .cols = "county") 
+  } 
+  if ("puma22" %in% names(data)) {
+    data <- data %>%
+      rename_with(~ifelse(. %in% "puma22", "puma.census.geoid", .),
+                  .cols = "puma22")
+  } 
+  return(data) }
+
+
+# Apply processing
+ct_tr_area <- process_data(ct_tr_area) 
+ct_tr_population <- process_data(ct_tr_population)
+tr_ct_population <- process_data(tr_ct_population)
+tr_ct_area <- process_data(tr_ct_area)
+pm_tr_population <- process_data(pm_tr_population)
+pm_tr_area <- process_data(pm_tr_area)
+tr_pm_population <- process_data(tr_pm_population)
+tr_pm_area <- process_data(tr_pm_area)
+
+# Crosswalk census function
+crosswalk_census <- function(data, 
+                             source_scale,
+                             target_scale,
+                             weight_by, 
+                             method,
+                             variable) {
+  if (!is.data.frame(data)) {
+    stop("Input 'data' must be a dataframe.")
+  }
+  
+  if (source_scale == "county.census.geoid" && target_scale == "tract.census.geoid") {
+    if (weight_by == "population") {
+      processed_data<-apply_weight(data = data,
+                                   method = method,
+                                   source_scale = source_scale,
+                                   weight_data = ct_tr_population, 
+                                   key = "county.census.geoid",
+                                   target_scale = "tract.census.geoid",
+                                   variable = variable,
+                                   weight_value = "afact")
+    } else if (weight_by == "area") {
+      processed_data<-apply_weight(data = data,
+                                   method = method,
+                                   source_scale = source_scale,
+                                   weight_data = ct_tr_area, 
+                                   key = "county.census.geoid",
+                                   target_scale = "tract.census.geoid",
+                                   variable = variable,
+                                   weight_value = "afact")
+    }
+  } else if (source_scale == "tract.census.geoid" && target_scale == "county.census.geoid") {
+    if (weight_by == "population") {
+      processed_data <- apply_weight(data = data,
+                                   method = method,
+                                   source_scale = source_scale,
+                                   weight_data = tr_ct_population, 
+                                   key = "tract.census.geoid",
+                                   target_scale = "county.census.geoid",
+                                   variable = variable,
+                                   weight_value = "afact")
+    } else if (weight_by == "area") {
+      processed_data<-apply_weight(data = data,
+                                   method = method,
+                                   source_scale = source_scale,
+                                   weight_data = tr_ct_population, 
+                                   key = "tract.census.geoid",
+                                   target_scale = "county.census.geoid",
+                                   variable = variable,
+                                   weight_value = "afact")
+    }
+  } else if (source_scale == "puma.census.geoid" && target_scale == "tract.census.geoid") {
+    if (weight_by == "population") {
+      processed_data<-apply_weight(data = data,
+                                   method = method,
+                                   source_scale = source_scale,
+                                   weight_data = pm_tr_population, 
+                                   key = "puma.census.geoid",
+                                   target_scale = "tract.census.geoid",
+                                   variable = variable,
+                                   weight_value = "afact")
+    } else if (weight_by == "area") {
+      processed_data<-apply_weight(data = data,
+                                   method = method,
+                                   source_scale = source_scale,
+                                   weight_data = pm_tr_area, 
+                                   key = "puma.census.geoid",
+                                   target_scale = "tract.census.geoid",
+                                   variable = variable,
+                                   weight_value = "afact")
+    }
+  } else if (source_scale == "tract.census.geoid" && target_scale == "puma.census.geoid") {
+    if (weight_by == "population") {
+      processed_data<-apply_weight(data = data,
+                                   method = method,
+                                   source_scale = source_scale,
+                                   weight_data = tr_pm_population, 
+                                   key = "tract.census.geoid",
+                                   target_scale = "puma.census.geoid",
+                                   variable = variable,
+                                   weight_value = "afact")
+    } else if (weight_by == "area") {
+      processed_data<-apply_weight(data = data,
+                                   method = method,
+                                   source_scale = source_scale,
+                                   weight_data = tr_pm_area, 
+                                   key = "tract.census.geoid",
+                                   target_scale = "puma.census.geoid",
+                                   variable = variable,
+                                   weight_value = "afact")
+    }
+  } else {
+    stop("Unsupported combination of source_scale, target_scale, and weight_by.")
+  }
+  
+  
+  return(processed_data)
+}
