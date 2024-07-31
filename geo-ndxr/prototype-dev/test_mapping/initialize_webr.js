1d3a7e0925fc7c9ae504e9c2fa089620a206229c
voglerdaniel <dvogler98@gmail.com>
2024-07-31 09:59:42 -0700

    Added JS to intialize WebR in background when website is launched

--- geo-ndxr/prototype-dev/test_mapping/initialize_webr.js
+++ geo-ndxr/prototype-dev/test_mapping/initialize_webr.js
@@ -1,8 +1,4 @@
-function printInput() {
-    var input = document.getElementById('userInput').value;
-    var outputDiv = document.getElementById('output');
-    outputDiv.innerHTML = "You entered: " + input;
-
+function initializeWebR() {
     // Import the webr module and then run the code
     import('https://webr.r-wasm.org/latest/webr.mjs').then(async ({ WebR }) => {
         // Wait for the WebR session to start
@@ -51,13 +47,6 @@ function printInput() {
         let scriptResponse = await fetch('r-file.R');
         let scriptText = await scriptResponse.text();
         await webr.evalR(scriptText);
-
-        // Execute the script and get the output
-        result = await webr.evalR(`func("${input}")`);
-        let output = (await result.toJs()).values;
-
-        // Display the output in the content element
-        outputDiv.innerHTML += "<br>R function output: " + output;
 
     }).catch((error) => {
         console.error("Failed to import the webr module:", error);
