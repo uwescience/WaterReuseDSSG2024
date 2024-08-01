5ebe8fe29d8f55545c1b2e6a1576d8f5cbad522f
Nora Povejsil <npovejsil@berkeley.edu>
2024-08-01 11:24:08 -0700

    fixed docstring

--- code/website_helper/edit_options.R
+++ code/website_helper/edit_options.R
@@ -1,6 +1,7 @@
 edit_options <- function(website_title, description, menu_options) {
   
-  # Edits website design options
+  #' Edits website design options
+  #'
   #' @description
     #' Input menu options to automatically update the template HTML file  
     #' 
@@ -11,14 +12,18 @@ edit_options <- function(website_title, description, menu_options) {
     #'  hope users can learn. 
     #' @param menu_options menu options are a list in a dictionary format from a  with a specific format. If you would like to group indicators (columns) 
     #' by a category (e.g "Environmental Stressors" or "Social Vulnerability"), you need to construct your string like this: 
+    #' \code{
     #' list(
-    #'    Environmental_Stressors = list(column1 = "Column 1", column2 = "Column 2"), 
-    #'    Social_Vulnerability = list(column3 = "Column 3", column4 = "Column 4"))
-    #'OR a simple list with the columns of interest:
-    #'list(column1 = "Column 1", column2 = "Column 2", column3 = "Column3", column4 = "Column4")
-    #'
-    #' @output
-    #'Overwrites options.json in the website folder to include your custom website options. 
+    #' Environmental_Stressors = list(column1 = "Column 1", column2 = "Column 2"), 
+    #' Social_Vulnerability = list(column3 = "Column 3", column4 = "Column 4"))
+    #' }
+    #' OR a simple list with the columns of interest:
+    #' \code{
+    #' list(column1 = "Column 1", column2 = "Column 2", column3 = "Column 3", column4 = "Column 4")
+    #' }
+  #'
+  #' @return Overwrites options.json in the website folder to include your custom website options.
+  #' @export
   
   require(rjson)
   
