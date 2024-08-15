4bb56b861dbc3b9ebb37d8e03c7e7b197a5c8b01
Jihyeon Bae <jihyeonb@uw.edu>
2024-08-15 14:19:24 -0700

    Formatting

--- geo-ndxr/prototype-dev/connecting-pieces/scripts/generate_txt.R
+++ geo-ndxr/prototype-dev/connecting-pieces/scripts/generate_txt.R
@@ -1,13 +1,32 @@
-# Function to generate metadata as a text file
-generate_txt <- function(data, author, contact) {
+generate_txt <- function(data, author, contact, sources) {
+  #' Function to generate metadata as a text file
+  #' @param data geojson dataset
+  #' @param author string. name of the author
+  #' @param contact string. contact information
+  #' @param sources list of data sources
+  #' @example generate_txt(data, author = "JB", 
+  #' contact = "jb_at_uw_edu", 
+  #' sources = list("source1", "source2"))
+  #' 
   metadata <- list(
-    dataset_name = deparse(substitute(data)),
-    author = author,
-    contact = contact,
-    column_names = names(data),
-    summary = summary(data)
+    "Dataset Name" = deparse(substitute(data)),
+    "Author" = author,
+    "Contact" = contact,
+    "Column Names" = names(data),
+    "Sources" = sources
   )
-  metadata_json <- jsonlite::toJSON(metadata, pretty = TRUE)
-  writeLines(metadata_json, "metadata_output.txt")
+  
+  formatted_metadata <- paste(
+    paste("Dataset Name:", metadata[["Dataset Name"]]),
+    paste("Author:", metadata[["Author"]]),
+    paste("Contact:", metadata[["Contact"]]),
+    "Column Names:",
+    paste(metadata[["Column Names"]], collapse = "\n"),
+    "Sources:",
+    paste(metadata[["Sources"]], collapse = "\n"),
+    sep = "\n\n"
+  )
+  
+  # Write the formatted metadata to a .txt file
+  writeLines(formatted_metadata, "metadata_output.txt")
 }
-
