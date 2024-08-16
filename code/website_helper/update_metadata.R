0581586e9d6ade570d759fea5fb5ff412f72bea1
voglerdaniel <dvogler98@gmail.com>
2024-08-16 11:48:57 -0700

    Attempted to replicate Curtis Rmd from 8/15

--- code/website_helper/update_metadata.R
+++ code/website_helper/update_metadata.R
@@ -1,17 +1,23 @@
-update_js_metadata <- function(geojson_data = NULL) {
-  library(jsonlite)
+update_js_metadata <- function(geojson_path = NULL, home_path) {
+  require(jsonlite)
+  print("Loaded dependency...")
   if (is.null(geojson_data) || !is.character(geojson_data)) {
     stop("Invalid input: geojson_data must be a non-null string.")
   }
-  geojson_data <- readLines()
+  print("Starting to read input JSON...")
+  geojson_data <- readLines(geojson_path)
   num_char <- nchar(geojson_data)
   byte_size <- nchar(geojson_data, type = "bytes")
-  
+
+  print("Updating metadata output...")
   updated_content <- sprintf(
     '{"files":[{"filename":"/abc.geojson","start":0,"end":%d}],"remote_package_size":%d}',
     num_char, byte_size
   )
+  print("Finished updating metadata output.")
 
-  jsonlite::write_json(updated_content, "geo-ndxr/website_materials/output.js.metadata")
+  print("Writing output...")
+  jsonlite::write_json(updated_content, paste0(home_path, "/geo-ndxr/website_materials/output.js.metadata"))
+  print("Complete.")
 }
 
